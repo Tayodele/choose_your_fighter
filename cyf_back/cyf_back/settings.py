652c67c0f95f71ed826bde1dc1a6d2e16bfc8076
TJ Ayodele <tjayodele@gmail.com>
2021-01-28 20:37:12 -0600

    Update settings.py

--- cyf_back/cyf_back/settings.py
+++ cyf_back/cyf_back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm2q4wt5@#ms2a$210v07c--&4w1j5kk#(tj7874212^u!v*m(m'
+SECRET_KEY = "D'oh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
